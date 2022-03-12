405d6791ff9879c3e0985431a03d5911ec83ff43
Samuel Searles-Bryant <devel@samueljsb.co.uk>
2022-03-12 16:18:37 +0000

    initial commit

---
+++ gh_notifs.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+import argparse
+import datetime
+import json
+from typing import Any
+from typing import Sequence
+
+import humanize
+import urllib3
+
+http = urllib3.PoolManager()
+
+NOTIFS_URL = "https://api.github.com/notifications"
+
+
+def get_data(url: str) -> Any:
+    resp = http.request("GET", url)
+    return json.loads(resp.data)
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--user",
+        metavar="<user:password>",
+        required=True,
+        dest="basic_auth",
+        help="Basic auth user and password",
+    )
+    parser.add_argument(
+        "--referrer-id",
+        dest="referrer_id",
+        help="Notification referrer ID",
+    )
+    parser.add_argument(
+        "--hide-closed",
+        action="store_true",
+        default=False,
+        help="Only show open PRs",
+    )
+    args = parser.parse_args(argv)
+
+    http.headers = urllib3.make_headers(basic_auth=args.basic_auth)
+
+    notifs = get_data(NOTIFS_URL)
+    print(f"There are {len(notifs)} unread notifications")
+    for notif in notifs:
+        subject_type = notif["subject"]["type"]
+        if subject_type != "PullRequest":
+            continue
+
+        subject_url = notif["subject"]["url"]
+        pr = get_data(subject_url)
+
+        if args.hide_closed and pr["state"] == "closed":
+            continue
+
+        if pr["state"] == "open":
+            if pr["draft"]:
+                status = "\x1b[2m[DRAFT] \x1b[0;1m"
+            else:
+                status = "\x1b[1m"
+        elif pr["state"] == "closed":
+            if pr["merged"]:
+                status = "\x1b[35m[MERGED] \x1b[0;2m"
+            else:
+                status = "\x1b[31m[CLOSED] \x1b[0;2m"
+        else:
+            status = f"[{pr['state'].upper()}] \x1b[1m"
+
+        updated_at = pr["updated_at"]
+        if updated_at.endswith("Z"):
+            updated_at = updated_at[:-1]
+        updated_at = datetime.datetime.fromisoformat(updated_at)
+
+        if args.referrer_id:
+            subject_url += f"?notification_referrer_id={args.referrer_id}"
+
+        print("---")
+        print(f"{status} {pr['title']}\x1b[0m ({pr['user']['login']})")
+        print(
+            "    "
+            f"{humanize.naturaltime(updated_at)} "
+            f"({pr['commits']} commits, {pr['changed_files']} files) "
+            f"[\x1b[32m+{pr['additions']}\x1b[0m \x1b[31m-{pr['deletions']}\x1b[0m] "
+        )
+        print("    " + subject_url)
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
