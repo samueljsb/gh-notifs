f14ebb6ea9e461b2b768450a2be26edd1565621c
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2024-02-27 15:17:04 +0000

    Upgradde Bootstrap

--- gh_notifs.py
+++ gh_notifs.py
@@ -366,8 +366,8 @@ class HtmlFormatter:
     <meta charset="utf-8"/>
     <title>GitHub PR notifications</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body class="bg-dark">
     <div class="container my-2 lh-1">
